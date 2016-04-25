fec18e46acda7e7940610650af056f41ce24bfc7
jbooher <john.c.booher@gmail.com>
2016-04-25 15:28:43 -0500

    Moved map JS to its own file and updated the map to remove UI.

--- app/app.js
+++ app/app.js
@@ -1,16 +1,27 @@
-import angular from 'angular';
-import uiRouter from 'angular-ui-router';
+let lat = 0;
+let lon = 0;
 
-import firebase from 'firebase';
-import angularFire from 'angularfire';
+navigator.geolocation.getCurrentPosition(function(position) {
+  lat = position.coords.latitude;
+  lon = position.coords.longitude;
 
-let App = angular.module('app', [
-  'ui.router',
-  'firebase'
-]);
+  const map = new google.maps.Map(document.querySelector('.mapDiv'), {
+    center: { lat: lat, lng: lon},
+    zoom: 12,
+    disableDefaultUI: true
+    });
 
-function config($urlRouterProvider) {
-  $urlRouterProvider.otherwise("/");
-}
+  let marker = new google.maps.Marker({ position: { lat: 34.747874, lng: -92.264993}, map: map, title: 'The Iron Yard' });
 
-App.config(config);
+  let infoWindow = new google.maps.InfoWindow({ content: `<h1>The Iron Yard</h1>` });
+
+  marker.addListener('click',
+    function() {
+      if (infoWindow.getMap()) {
+        infoWindow.close();
+      }
+      else {
+        infoWindow.open(map, marker);
+      }
+    });
+});
